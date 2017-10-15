4a1356c4a7e899ff3e434f82359f69f0e7dc3dec
axtk <midwinterday@inbox.ru>
2017-10-15 16:17:19 +0300

    added main script

---
+++ index.js
@@ -0,0 +1,84 @@
+;(function() {
+
+if (window.tyler) return;
+
+const EVENT_NAMESPACE = 'tyler/';
+const EVENT_SOURCE_SELF_REFERENCE = './';
+
+var dispatcher = {
+    fire(event, options) {
+        if (this.interceptionCallback) this.interceptionCallback(event, options);
+        window.dispatchEvent(new CustomEvent(EVENT_NAMESPACE + event, { detail: options }));
+        return this;
+    },
+    off(event, handler) {
+        window.removeEventListener(EVENT_NAMESPACE + event, handler);
+        return this;
+    },
+    on(event, handler, context) {
+        if (event instanceof Array)
+            return event.forEach(item => this.on(item, handler, context)) || this;
+
+        if (event.indexOf(EVENT_SOURCE_SELF_REFERENCE) === 0) {
+            let originalHandler = handler;
+            event = event.substring(EVENT_SOURCE_SELF_REFERENCE.length);
+            handler = function(e, options) {
+                if (options && options.source === context) originalHandler.call(context, e, options);
+            };
+        }
+        else if (context)
+            handler = handler.bind(context);
+
+        window.addEventListener(EVENT_NAMESPACE + event, e => handler(e, e.detail), false);
+        return this;
+    },
+    intercept(callback) {
+        this.interceptionCallback = callback;
+        return this;
+    }
+};
+
+var Observer = function(extension) {
+    Object.assign(this, extension || {});
+};
+
+Object.assign(Observer.prototype, {
+    fire(event, options) {
+        dispatcher.fire(event, Object.assign({ source: this }, options));
+        return this;
+    },
+    fires(event, options) { // a handy shortcut for DOM event handlers
+        return () => this.fire(event, options);
+    },
+    on(event, handler) {
+        dispatcher.on(event, handler, this);
+        return this;
+    }
+});
+
+var toDOM = html => new DOMParser().parseFromString(html, 'text/html').body.childNodes;
+
+var createElement = extension => {
+    var Element = function(node) {
+        Observer.call(this, extension);
+        if (typeof node === 'string')
+            this.node = toDOM(node)[0];
+        else if (node)
+            this.node = node;
+        Element.initialHandlers.forEach(item => this.on(item.event, item.handler));
+    };
+
+    Element.prototype = Object.create(Observer.prototype);
+    Element.initialHandlers = [];
+
+    Element.on = (event, handler) => {
+        Element.initialHandlers.push({ event, handler });
+        return Element;
+    };
+
+    return Element;
+};
+
+window.tyler = { Observer, Service: Observer, createElement, dispatcher, toDOM };
+
+})();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# tyler
+A JS framework for building clean loosely coupled self-contained web app components, or tiles
